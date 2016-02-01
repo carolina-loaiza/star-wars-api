313d90a008bc163172d1a0ed3eee66fa69e7f18f
LauGon <LauGon>
2016-02-01 08:44:12 -0600

    modified gallery grid

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,33 @@
 var gridShips = (function() {
-  
-
+  var containerGrid = $('#prueba');
 
   function printGallery(data) {
+    var cont = 0;
+    var content;
+    var d;
+    var dShip;
+    var shipName;
+    var grid = '<div class="row">';
 
     for (i = 0; i < 6; i++) { 
+      cont ++;
+      d = data.results[i].url;
+      dShip = d.match(/\d+/)[0];
+      shipName = data.results[i].name;      
+      content = '<div class="col-md-4 ships-item ship-'+dShip+'" data-type="ship-'+dShip+'"><h3>'+shipName+'</h3></div>';
+      grid += content;
+
+      if(cont%3 == 0){
+        containerGrid.append(grid);
+      }
       //console.log(data.results[i].name)
-      var d = data.results[i].url
       //console.log(d.match(/\d+/)[0])
+      //containerGrid.append(content);
     };
+
+    grid += '</div>';
+
+    //containerGrid.append(grid);
   }
 
   return {
